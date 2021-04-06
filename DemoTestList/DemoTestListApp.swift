057d1e3b1a7dd99a0d9e8ed31767dd6f6393c3f7
Pascal Rocq <pascal.rocq+git@gmail.com>
2021-04-06 22:59:07 +0200

    Initial Commit

---
+++ DemoTestList/DemoTestListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoTestListApp.swift
+//  DemoTestList
+//
+//  Created by Pascal Rocq on 06/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct DemoTestListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
